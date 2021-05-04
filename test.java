131dd540526a719390c8e91f2e944bcb20e68291
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-05-04 09:26:26 +0200

    sdfasdf

---
+++ test.java
@@ -0,0 +1,263 @@
+/*
+ * 		file.Classes[0].Name	"Pizza"	string
+-		file.Operands	Count = 192	System.Collections.Generic.List<string>
+		[0]	"package"	string
+		[1]	"basic"	string
+		[2]	"."	string
+		[3]	"classes"	string
+		[4]	"."	string
+		[5]	"enums"	string
+		[6]	";"	string
+		[7]	"public"	string
+		[8]	"class"	string
+		[9]	"Pizza"	string
+		[10]	"{"	string
+		[11]	"private"	string
+		[12]	"PizzaStatus"	string
+		[13]	"status"	string
+		[14]	";"	string
+		[15]	"public"	string
+		[16]	"enum"	string
+		[17]	"PizzaStatus"	string
+		[18]	"{"	string
+		[19]	"ORDERED"	string
+		[20]	"("	string
+		[21]	"5"	string
+		[22]	")"	string
+		[23]	"{"	string
+		[24]	"@"	string
+		[25]	"Override"	string
+		[26]	"public"	string
+		[27]	"boolean"	string
+		[28]	"isOrdered"	string
+		[29]	"("	string
+		[30]	")"	string
+		[31]	"{"	string
+		[32]	"return"	string
+		[33]	"true"	string
+		[34]	";"	string
+		[35]	"}"	string
+		[36]	"}"	string
+		[37]	","	string
+		[38]	"READY"	string
+		[39]	"("	string
+		[40]	"2"	string
+		[41]	")"	string
+		[42]	"{"	string
+		[43]	"@"	string
+		[44]	"Override"	string
+		[45]	"public"	string
+		[46]	"boolean"	string
+		[47]	"isReady"	string
+		[48]	"("	string
+		[49]	")"	string
+		[50]	"{"	string
+		[51]	"return"	string
+		[52]	"true"	string
+		[53]	";"	string
+		[54]	"}"	string
+		[55]	"}"	string
+		[56]	","	string
+		[57]	"DELIVERED"	string
+		[58]	"("	string
+		[59]	"0"	string
+		[60]	")"	string
+		[61]	"{"	string
+		[62]	"@"	string
+		[63]	"Override"	string
+		[64]	"public"	string
+		[65]	"boolean"	string
+		[66]	"isDelivered"	string
+		[67]	"("	string
+		[68]	")"	string
+		[69]	"{"	string
+		[70]	"return"	string
+		[71]	"true"	string
+		[72]	";"	string
+		[73]	"}"	string
+		[74]	"}"	string
+		[75]	";"	string
+		[76]	"private"	string
+		[77]	"int"	string
+		[78]	"timeToDelivery"	string
+		[79]	";"	string
+		[80]	"public"	string
+		[81]	"boolean"	string
+		[82]	"isOrdered"	string
+		[83]	"("	string
+		[84]	")"	string
+		[85]	"{"	string
+		[86]	"return"	string
+		[87]	"false"	string
+		[88]	";"	string
+		[89]	"}"	string
+		[90]	"public"	string
+		[91]	"boolean"	string
+		[92]	"isReady"	string
+		[93]	"("	string
+		[94]	")"	string
+		[95]	"{"	string
+		[96]	"return"	string
+		[97]	"false"	string
+		[98]	";"	string
+		[99]	"}"	string
+		[100]	"public"	string
+		[101]	"boolean"	string
+		[102]	"isDelivered"	string
+		[103]	"("	string
+		[104]	")"	string
+		[105]	"{"	string
+		[106]	"return"	string
+		[107]	"false"	string
+		[108]	";"	string
+		[109]	"}"	string
+		[110]	"public"	string
+		[111]	"int"	string
+		[112]	"getTimeToDelivery"	string
+		[113]	"("	string
+		[114]	")"	string
+		[115]	"{"	string
+		[116]	"return"	string
+		[117]	"timeToDelivery"	string
+		[118]	";"	string
+		[119]	"}"	string
+		[120]	"PizzaStatus"	string
+		[121]	"("	string
+		[122]	"int"	string
+		[123]	"timeToDelivery"	string
+		[124]	")"	string
+		[125]	"{"	string
+		[126]	"this"	string
+		[127]	"."	string
+		[128]	"timeToDelivery"	string
+		[129]	"timeToDelivery"	string
+		[130]	";"	string
+		[131]	"}"	string
+		[132]	"}"	string
+		[133]	"public"	string
+		[134]	"boolean"	string
+		[135]	"isDeliverable"	string
+		[136]	"("	string
+		[137]	")"	string
+		[138]	"{"	string
+		[139]	"return"	string
+		[140]	"this"	string
+		[141]	"."	string
+		[142]	"status"	string
+		[143]	"."	string
+		[144]	"isReady"	string
+		[145]	"("	string
+		[146]	")"	string
+		[147]	";"	string
+		[148]	"}"	string
+		[149]	"public"	string
+		[150]	"void"	string
+		[151]	"printTimeToDeliver"	string
+		[152]	"("	string
+		[153]	")"	string
+		[154]	"{"	string
+		[155]	"System"	string
+		[156]	"."	string
+		[157]	"out"	string
+		[158]	"."	string
+		[159]	"println"	string
+		[160]	"("	string
+		[161]	"\"Time to delivery is \""	string
+		[162]	"this"	string
+		[163]	"."	string
+		[164]	"getStatus"	string
+		[165]	"("	string
+		[166]	")"	string
+		[167]	"."	string
+		[168]	"getTimeToDelivery"	string
+		[169]	"("	string
+		[170]	")"	string
+		[171]	")"	string
+		[172]	";"	string
+		[173]	"}"	string
+		[174]	"basic"	string
+		[175]	"."	string
+		[176]	"classes"	string
+		[177]	"."	string
+		[178]	"enums"	string
+		[179]	"."	string
+		[180]	"Pizza2"	string
+		[181]	"."	string
+		[182]	"PizzaStatus"	string
+		[183]	"getStatus"	string
+		[184]	"("	string
+		[185]	")"	string
+		[186]	"{"	string
+		[187]	"return"	string
+		[188]	"null"	string
+		[189]	";"	string
+		[190]	"}"	string
+		[191]	"}"	string
++		Raw View		
+-		file.Operators	Count = 2	System.Collections.Generic.List<string>
+		[0]	"="	string
+		[1]	"+"	string
++		Raw View		
+		file.Node.EntityId	-1427084109	long
+ */
+package basic.classes.enums;
+
+public class Pizza {
+	private PizzaStatus status;
+
+	public enum PizzaStatus {
+		ORDERED(5) {
+			@Override
+			public boolean isOrdered() {
+				return true;
+			}
+		},
+		READY(2) {
+			@Override
+			public boolean isReady() {
+				return true;
+			}
+		},
+		DELIVERED(0) {
+			@Override
+			public boolean isDelivered() {
+				return true;
+			}
+		};
+
+		private int timeToDelivery;
+
+		public boolean isOrdered() {
+			return false;
+		}
+
+		public boolean isReady() {
+			return false;
+		}
+
+		public boolean isDelivered() {
+			return false;
+		}
+
+		public int getTimeToDelivery() {
+			return timeToDelivery;
+		}
+
+		PizzaStatus(int timeToDelivery) {
+			this.timeToDelivery = timeToDelivery;
+		}
+	}
+
+	public boolean isDeliverable() {
+		return this.status.isReady();
+	}
+
+	public void printTimeToDeliver() {
+		System.out.println("Time to delivery is " + this.getStatus().getTimeToDelivery());
+	}
+
+	basic.classes.enums.Pizza2.PizzaStatus getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
